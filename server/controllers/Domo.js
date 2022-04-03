7a3116f4183aa0c49486f77b395b4cac5a76d1d1
notsummerkarnani <93565277+notsummerkarnani@users.noreply.github.com>
2022-04-03 19:08:53 -0400

    part D complete

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,19 +1,9 @@
 const models = require('../models');
+const DomoModel = require('../models/Domo');
 
 const { Domo } = models;
 
-const makerPage = (req, res) => {
-  Domo.findByOwner(req.session.account._id, (err, docs) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).json({ error: 'An error has occured' });
-    }
-    return res.render('app', {
-      csrfToken: req.csrfToken(),
-      domos: docs,
-    });
-  });
-};
+const makerPage = (req, res) => res.render('app');
 
 const makeDomo = async (req, res) => {
   if (!req.body.name || !req.body.age) {
@@ -29,7 +19,7 @@ const makeDomo = async (req, res) => {
   try {
     const newDomo = new Domo(domoData);
     await newDomo.save();
-    return res.json({ redirect: '/maker' });
+    return res.status(201).json({ name: newDomo.name, age: newDomo.age });
   } catch (err) {
     if (err.code === 11000) {
       return res.status(400).json({ error: 'Domo already exists!' });
@@ -37,7 +27,15 @@ const makeDomo = async (req, res) => {
     return res.status(400).json({ error: 'An error occured' });
   }
 };
+const getDomos = (req, res) => DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+  if (err) {
+    console.log(err);
+    return res.status(400).json({ error: 'An error occured!' });
+  }
+  return res.json({ domos: docs });
+});
 module.exports = {
   makerPage,
   makeDomo,
+  getDomos,
 };
